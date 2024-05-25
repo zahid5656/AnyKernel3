ac2301640ade4255f65f01639d341e41fa900f36
zahid5656 <74774199+zahid5656@users.noreply.github.com>
2024-05-26 03:11:02 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -34,4 +34,13 @@ split_boot;
 flash_boot;
 flash_dtbo;
 
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+#set_perm_recursive 0 0 755 644 $ramdisk/*;
+#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+
+## AnyKernel boot install
+#dump_boot;
+#write_boot;
+
 ## end install
