915272713008a94d42e41ac8beb2b7624b51722f
zahid5656 <74774199+zahid5656@users.noreply.github.com>
2024-07-10 08:51:55 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Hyper Kernel for Realme X2 Pro
+kernel.string=HyperTeam Openela Kernel for Realme X2 Pro
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -25,17 +25,10 @@ ramdisk_compression=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
-# AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-#set_perm_recursive 0 0 755 644 $ramdisk/*;
-#set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
 ## AnyKernel boot install
 split_boot;
-
 flash_boot;
 flash_dtbo;
-
 
 ## AnyKernel boot install
 #dump_boot;
@@ -44,5 +37,6 @@ flash_dtbo;
 ## end install
 
 sleep 1
-ui_print ""
-ui_print "kernel installed successfully"
+ui_print " "
+ui_print "DONE!"
+ui_print "Kernel Installed Successfully !!!"
