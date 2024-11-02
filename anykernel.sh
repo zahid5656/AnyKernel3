6ea0cda4136d20883a169cc1060814b540f26727
zahid5656 <74774199+zahid5656@users.noreply.github.com>
2024-11-03 03:17:06 +0600

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=HyperTeam Openela Kernel for Realme X2 Pro
+kernel.string=Openela-LTS Kernel for Realme X2 Pro
 do.devicecheck=1
 do.modules=0
 do.systemless=1
