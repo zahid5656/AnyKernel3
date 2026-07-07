d1fdad216f7ac75d6d38fedbe829d8ff438ccfd3
MD. ZAHIDUL ISLAM <74774199+zahid5656@users.noreply.github.com>
2026-07-07 06:29:22 +0600

    Modify anykernel.sh for OpenELA-LTS Kernel installation
    Updated boot script for OpenELA-LTS Kernel installation with improved UI messages and added patch vbmeta flag.

--- anykernel.sh
+++ anykernel.sh
@@ -2,7 +2,6 @@
 ## osm0sis @ xda-developers
 
 ### AnyKernel setup
-# global properties
 properties() { '
 kernel.string=OpenELA-LTS Kernel for Realme X2 Pro
 do.devicecheck=1
@@ -14,25 +13,34 @@ device.name1=samurai
 device.name2=RMX1931
 device.name3=RMX1931CN
 supported.versions=
+supported.patchlevels=
 '; } # end properties
 
 # boot shell variables
-BLOCK=/dev/block/by-name/boot;
-IS_SLOT_DEVICE=0;
-RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
+block=/dev/block/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+# import functions/variables and setup patching - DO NOT REMOVE
 . tools/ak3-core.sh;
 
-## AnyKernel boot install
-split_boot;
-flash_boot;
-flash_dtbo;
+ui_print "*******************************"
+ui_print " OpenELA-LTS Kernel"
+ui_print " Realme X2 Pro / Samurai"
+ui_print " KernelSU-Next Test Build"
+ui_print "*******************************"
 
-## AnyKernel boot install
-#dump_boot;
-#write_boot;
+ui_print "- Dumping current boot image..."
+dump_boot;
+
+ui_print "- Flashing kernel while preserving ROM ramdisk..."
+write_boot;
+
+# IMPORTANT:
+# Do NOT flash dtbo during first boot test.
+# Wrong dtbo can cause boot-loop or black screen.
+# flash_dtbo;
 
 sleep 1
 ui_print "DONE!"
